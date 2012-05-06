7f713eb186f038c791c14d37d60a3f5d0976e16c
saintbyte <admin@dotsb.net.ru>
2012-05-06 19:53:22 +0600

    Можно собирать основные файлы в один файл. А потом можно кормить этот файл компрессорам =))

---
+++ build/make_all_in_one_1.cmd
@@ -0,0 +1,17 @@
+
+
+echo /* --- */ >  1.js
+type ..\panojs\utils.js >> 1.js
+echo /* --- */ >>  1.js
+type ..\panojs\PanoJS.js >> 1.js
+echo /* --- */ >>  1.js
+type ..\panojs\controls.js >> 1.js
+echo /* --- */ >>  1.js
+type ..\panojs\pyramid_imgcnv.js >> 1.js
+echo /* --- */ >>  1.js
+type ..\panojs\control_thumbnail.js >> 1.js
+echo /* --- */ >>  1.js
+type ..\panojs\control_info.js >> 1.js
+echo /* --- */ >>  1.js
+type ..\panojs\control_svg.js >> 1.js
+
